ac03ac64289dbef0e85b12c9ba34d67285f68446
Chris Hapner <hpaner.10@wright.edu>
2016-11-17 14:35:06 -0500

    Message 201 has initilization code now.

--- android/crowningkingsbase/src/main/java/edu/wright/crowningkings/base/ServerMessage/_201MessageRecieved.java
+++ android/crowningkingsbase/src/main/java/edu/wright/crowningkings/base/ServerMessage/_201MessageRecieved.java
@@ -8,11 +8,21 @@ import edu.wright.crowningkings.base.BaseClient;
 
 public class _201MessageRecieved extends AbstractServerMessage {
     public _201MessageRecieved(String message) {
-		setParameters(message.split(" "));
+		String[] param = message.split(" ");
+		String msgID = param[0];
+		String from = param[1];
+		String privateMessage = param[2];
+		String msg = "";
+		for (int i = 3; i < param.length; i++) {
+			msg = msg + " " + param[i];
+		}
+		String[] fullParams = {msgID, from, Private, msg};
+		setParameters(fullParams);
     }
 
     public void run(BaseClient client) {
 		System.out.println("_201MessageRecieved.run(BaseClient)");
+		//Add logic to say that param[2] is true if == 1
 		//do something
     }
 }
