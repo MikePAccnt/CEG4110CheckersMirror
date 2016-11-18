ae197afc1eb6848a348429695713053fc00e2f67
Chris Hapner <hpaner.10@wright.edu>
2016-11-17 22:17:17 -0500

    OpponentMove run is implemented.

--- android/crowningkingsbase/src/main/java/edu/wright/crowningkings/base/ServerMessage/_206OpponentMove.java
+++ android/crowningkingsbase/src/main/java/edu/wright/crowningkings/base/ServerMessage/_206OpponentMove.java
@@ -10,14 +10,18 @@ public class _206OpponentMove extends AbstractServerMessage {
     public _206OpponentMove(String message) {
         String[] param = message.split(" ");
 		String msgID = param[0];
-		String from = param[1];
-		String to = param[2];
-		String[] fullParam = {msgID, from, to};
+		String fromx = param[1];
+		String fromy = param[2];
+		String tox = param[3];
+		String toy = param[4];
+		String[] fullParam = {msgID, fromx, fromy, tox, toy};
         setParameters(fullParam);
     }
 
     public void run(BaseClient client) {
-		System.out.println("_206OpponentMove.run(BaseClient)");
-		//do something
+		//System.out.println("_206OpponentMove.run(BaseClient)");
+		String[] p1 = {getParameters()[1], getParameters()[2]};
+		String[] p2 = {getParameters()[3], getParameters()[4]};
+		client.opponentMove(p1, p2);
     }
 }
